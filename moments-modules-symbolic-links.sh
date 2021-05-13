9871fcc8cf8f736a4c4a93b8c9d76f6aa101fdf1
Caitlin J. Curry <ThatLionLady@users.noreply.github.com>
2021-05-13 15:58:25 -0700

    Update moments-modules-symbolic-links.sh

--- moments-modules-symbolic-links.sh
+++ moments-modules-symbolic-links.sh
@@ -3,7 +3,7 @@
 MPDIR=$1 # path to moments_pipeline directory
 WKDIR=$2 # path to working directory
 
-ln -s ${MPDIR}/Two_Population_Pipeline/Models_2D.py ${WKDIR}/Models_2D.py
-ln -s ${MPDIR}/Two_Population_Pipeline/Summarize_Outputs.py ${WKDIR}/Summarize_Outputs.py
-ln -s ${MPDIR}/Optimize_Functions.py Optimize_${WKDIR}/Functions.py
-ln -s ${MPDIR}/Goodness_of_Fit/Optimize_Functions_GOF.py ${WKDIR}/Optimize_Functions_GOF.py
+ln -s ${MPDIR}Two_Population_Pipeline/Models_2D.py ${WKDIR}Models_2D.py
+ln -s ${MPDIR}Two_Population_Pipeline/Summarize_Outputs.py ${WKDIR}Summarize_Outputs.py
+ln -s ${MPDIR}Optimize_Functions.py ${WKDIR}Optimize_Functions.py
+ln -s ${MPDIR}Goodness_of_Fit/Optimize_Functions_GOF.py ${WKDIR}Optimize_Functions_GOF.py
